85f963d95befa492e01c0f8c359170ec05e4d72f
Dagmara <dagmara.szczepanczyk@gmail.com>
2020-01-31 15:43:53 +0100

    added header component and links to home, shop page and contact

---
+++ .history/src/components/header/header.component_20200131153333.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.styles.scss';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+const Header = () => (
+  <div className='header'>
+    <Link to='/'>
+
+    </Link>
+  </div>
+)
