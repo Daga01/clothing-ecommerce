a0af66a91f2b86ed623d52121efc7849de7f1c0f
Dagmara <dagmara.szczepanczyk@gmail.com>
2020-02-04 13:00:06 +0100

    add stripe button to the checkout page

--- .history/src/pages/checkout/checkout.component_20200204123805.jsx
+++ .history/src/pages/checkout/checkout.component_20200204123805.jsx
@@ -35,11 +35,6 @@ const CheckoutPage = ({ cartItems, total }) => (
             <span>TOTAL: ${total}</span>
         </div>
         <StripeCheckoutButton price={total} />
-        <div className='test-warning'>
-        *Please use the following test credit card for payments
-        <br/>
-        4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
-        </div>
     </div>
 );
 
--- .history/src/pages/checkout/checkout.component_20200204125217.jsx
+++ .history/src/pages/checkout/checkout.component_20200204125217.jsx
@@ -34,12 +34,12 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
-        <StripeCheckoutButton price={total} />
         <div className='test-warning'>
         *Please use the following test credit card for payments
         <br/>
         4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
         </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
